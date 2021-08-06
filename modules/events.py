7552c0c8a2f4c0e485816926ee4194aca87d612c
jotte <71946106+jottew@users.noreply.github.com>
2021-08-06 15:54:32 +0200

    Added a repost checker for #doggo-of-the-day

--- modules/events.py
+++ modules/events.py
@@ -1,7 +1,9 @@
-import discord
+import imagehash
 import core
 import re
 
+from PIL import Image
+from io import BytesIO
 from discord.ext import commands
 
 
@@ -9,9 +11,32 @@ class Events(core.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.REGEX = re.compile(r"::([a-zA-B1-9]{2,32})")
+        self.images = []
 
-    @commands.Cog.listener()
-    async def on_message(self, msg):
+    @commands.Cog.listener("on_message")
+    async def similar(self, msg):
+        if msg.channel.id != 722249103794372648:
+            return
+
+        if not msg.attachments:
+            return
+
+        for attachment in msg.attachments:            
+            img = Image.open(BytesIO(await attachment.read()))
+            hash = imagehash.average_hash(img)
+
+            sent = False
+            for i in self.images:
+                if (i - hash) < 5:
+                    if not sent:
+                        await msg.reply("Repost bad!!!")
+                        sent = True
+            if not sent:
+                self.images.append(hash)
+            img.close()
+
+    @commands.Cog.listener("on_message")
+    async def emojis(self, msg):
         matches = re.findall(self.REGEX, msg.content)
 
         if matches:
